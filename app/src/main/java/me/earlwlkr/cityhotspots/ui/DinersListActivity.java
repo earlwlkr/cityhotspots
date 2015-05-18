ef016da352ab0a3162201a53eab533ec5ccac125
earlwlkr <mike358015@gmail.com>
2015-05-18 22:45:43 +0700

    Add 'View on GMap' action in ActionBar

--- app/src/main/java/me/earlwlkr/cityhotspots/ui/DinersListActivity.java
+++ app/src/main/java/me/earlwlkr/cityhotspots/ui/DinersListActivity.java
@@ -1,5 +1,7 @@
 package me.earlwlkr.cityhotspots.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
@@ -14,14 +16,14 @@ import me.earlwlkr.cityhotspots.R;
 import me.earlwlkr.cityhotspots.models.Diner;
 
 public class DinersListActivity extends FragmentActivity {
-
+    private List<Diner> mDinersList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diners_list);
         Bundle bundle = this.getIntent().getExtras();
         List<Diner> diners = Parcels.unwrap(bundle.getParcelable("diners"));
-
+        mDinersList = diners;
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
@@ -47,6 +49,13 @@ public class DinersListActivity extends FragmentActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.map:
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:0,0?q=" +
+                        Uri.encode(mDinersList.get(0).getAddress().getStreetAddress() +
+                        mDinersList.get(0).getAddress().getDistrict())));
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
