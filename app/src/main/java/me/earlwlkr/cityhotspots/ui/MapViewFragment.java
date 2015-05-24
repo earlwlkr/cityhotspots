5e821e02ec4976e999ffb9c2be1ad7b6b208b8d3
earlwlkr <mike358015@gmail.com>
2015-05-24 17:29:34 +0700

    Shows map with current location

--- app/src/main/java/me/earlwlkr/cityhotspots/ui/MapViewFragment.java
+++ app/src/main/java/me/earlwlkr/cityhotspots/ui/MapViewFragment.java
@@ -1,10 +1,20 @@
 package me.earlwlkr.cityhotspots.ui;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.parceler.Parcels;
 
@@ -14,9 +24,34 @@ import me.earlwlkr.cityhotspots.R;
 import me.earlwlkr.cityhotspots.models.Diner;
 
 /**
- * A placeholder fragment containing a simple view.
+ * Show list of places on map for user to pick.
  */
 public class MapViewFragment extends Fragment {
+
+    MapView mapView;
+    GoogleMap mMap;
+    Location mLastLocation;
+    LocationManager mLocationManager;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            mLastLocation = location;
+            if (mMap != null)
+            {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastLocation.getLatitude(),
+                        mLastLocation.getLongitude()), 17));
+            }
+        }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+    };
 
     public static MapViewFragment createInstance(List<Diner> diners) {
         MapViewFragment fragment = new MapViewFragment();
@@ -26,12 +61,52 @@ public class MapViewFragment extends Fragment {
         return fragment;
     }
 
-    public MapViewFragment() {
-    }
+    public MapViewFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map_view, container, false);
+        View v = inflater.inflate(R.layout.fragment_map_view, container, false);
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) v.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mMap = mapView.getMap();
+        mMap.setMyLocationEnabled(true);
+
+        mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 200l, 500.0f, mLocationListener);
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+        if (mLastLocation != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastLocation.getLatitude(),
+                    mLastLocation.getLongitude()), 17));
+        }
+
+        return v;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
